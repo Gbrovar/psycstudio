54e7d4d2d9dc29cee3997da90f51d8458e3b3a21
gbrovar <66496340+Gbrovar@users.noreply.github.com>
2023-02-26 15:41:11 +0000

    Update app.routing.ts

--- psycstudio/src/app/app.routing.ts
+++ psycstudio/src/app/app.routing.ts
@@ -1,1 +1,15 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './components/login/login.component'; 
+import { TherapistRegisterComponent } from './components/therapist-register/therapist-register.component'; 
+import { UserRegisterComponent } from './components/user-register/user-register.component'; 
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoguinComponent },
+  { path: 'therapist-register', component: TherapistRegisterComponent },
+  { path: 'user-register', component: UserRegisterComponent },
+]; 
+
+export const appRoutingProviders: any[] = [];
+export const: routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
